966bb8ae03a456942c481114aa0af1fce7da6af8
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-11-09 13:54:49 -0500

    file structure and preliminary model associations set up

---
+++ models/index.js
@@ -0,0 +1,23 @@
+// An index file to declare relationships between models and export them for use
+
+// Dependencies
+const Event = require('./Event');
+const Task = require('./Task');
+const User = require('./User');
+
+// Associations
+User.hasMany(Event, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Task, {
+    foreignKey: 'user_id'
+});
+
+Event.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Task.belongsTo(User, {
+    foreignKey: 'user_id'
+})
---
+++ server.js
@@ -0,0 +1,1 @@
+// Server for Week-In-View
