7be5c881a165c402d54193f66cc04b9e611ab815
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-11-15 11:37:02 -0500

    added task column to homepage and form for adding new task; new task form successfully adds task to database - will need to update api route to include the user id from the session

---
+++ public/javascript/add-task.js
@@ -0,0 +1,28 @@
+// New Task Form Handler
+async function newTaskFormHandler(event) {
+    event.preventDefault();
+
+    // Get the task text from the form
+    const text = document.querySelector('input[name="task-text"]').value.trim();
+
+    // Use the add new task POST route to add the task to the database
+    const response = await fetch('api/tasks', {
+        method: 'POST',
+        body: JSON.stringify({
+            text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+          }       
+    });
+
+    // if the response is ok, reload the page, showing the task in the task list; otherwise, display the error
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText)
+    }
+}
+
+// Event Listener for the submit button
+document.querySelector('.new-task-form').addEventListener('submit', newTaskFormHandler)
